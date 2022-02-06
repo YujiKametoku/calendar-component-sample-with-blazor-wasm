7bc97aa56bf67df50deca3543b13df71fa622afd
yuji kametoku <yuji.kametoku@gmail.com>
2022-02-06 11:49:04 +0900

    プロジェクト ファイルを追加します。

---
+++ calendar_component_sample/Components/MyCalendar/Date.cs
@@ -0,0 +1,14 @@
+﻿namespace calendar_component_sample.Components.MyCalendar
+{
+	public class Date
+	{
+		/// <summary>カレンダーの日付</summary>
+		public DateTime DateTime { get; set; }
+
+		/// <summary>HTMLのclass</summary>
+		public string Class { get; set; } = "";
+
+		/// <summary>HTMLのid</summary>
+		public string Id { get; set; } = "";
+	}
+}
